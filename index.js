ee769d236f27ed868aabe07daf462806bfbeadc5
DanielWinebrenner <winebrenner444@yahoo.com>
2023-11-02 14:14:07 -0700

    Updated hamburger drop down menu

--- index.js
+++ index.js
@@ -1,12 +1,25 @@
 let hamburgerMenu = document.querySelector('.hamburger-menu');
 let navList = document.querySelector('.nav-list');
-
+var navItems = document.querySelectorAll('.nav-item');
 
 
 
 function toggleNavList() {
     if (navList.style.display === 'none' || navList.style.display === '') {
-        navList.style.display = 'block';
+        navList.style.display = 'flex';
+        navList.style.width = "100vw";
+        navList.style.height = "100vh";
+        navList.style.right = "-20px";
+        navList.style.top = "-20px";
+        navList.style.alignItems = "center";
+        navList.style.justifyContent = "center";  
+        navList.style.fontSize = "5vw"; 
+        
+            for (var i = 0; i < navItems.length; i++) {
+                navItems[i].style.margin = "3vh 0";
+                navItems[i].style.width = "50vw";
+            }
+
     } else {
         navList.style.display = 'none';
     }
